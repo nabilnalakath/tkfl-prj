cf9b296eecf2bf0eefbb5e7e227e75457e58fd5f
Nabil Mohammed Nalakath <nabilnalakath.git@gmail.com>
2025-04-15 02:25:51 +0530

    Merge to main branch

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,17 +9,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// We extend the recommended Next.js and TypeScript configs,
-// then override certain rules to disable no-unused-vars & no-explicit-any.
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "react/no-unescaped-entities": "off"
-    },
-  },
+  ...compat.extends("next/core-web-vitals", "next/typescript")
 ];
 
 export default eslintConfig;
