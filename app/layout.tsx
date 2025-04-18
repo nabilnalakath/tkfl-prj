c9e3443173feda01eaf7ed298035ee98fd4aa802
Nabil Mohammed Nalakath <nabilnalakath.git@gmail.com>
2025-04-19 00:20:53 +0530

    Added meta tags on home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
+// app/layout.tsx
 import "./globals.css";
 import { CurrencyProvider } from "@/context/CurrencyContext";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Crypto Dashboard",
+  description:
+    "Browse the top cryptocurrencies by market cap—live prices, sorting, filtering, and detailed views.",
+};
 
 export default function RootLayout({
   children,
@@ -7,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="en">
       <body>
         <CurrencyProvider>{children}</CurrencyProvider>
       </body>
